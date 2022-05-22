7dfd8819594db406504a156cf697536cade94b5c
alaille <99029042+alaille@users.noreply.github.com>
2022-05-22 16:20:43 +0300

    Fix codestyle in geometry

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -1,15 +1,15 @@
-    #include <ctype.h>
-    #include <math.h>
-    #include <stdbool.h>
-    #include <stdio.h>
-    #include <stdlib.h>
-    #include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-    #include "libgeometry/lib_calc_trans.h"
-    #include "libgeometry/lib_main_chek.h"
+#include "lib/lib_calc_trans.h"
+#include "lib/libmainchek.h"
 
-    int main()
-    {
+int main()
+{
     int max = 30;
     char str1[max];
     char str2[max];
@@ -21,13 +21,13 @@
     printf("For first circle: ");
     fgets(str1, max, stdin);
     if (isalpha(*mark1) != 0) {
-    while (isalpha(*mark2) != 0)
-    mark2++;
+        while (isalpha(*mark2) != 0)
+            mark2++;
     } 
     if (mainchek(mark1, wr) == 1)
-    return 0;
+        return 0;
     else
-    mark1 = mark2;
+        mark1 = mark2;
     translate(mark1, d1);
     calculation(d1);
 
@@ -37,13 +37,13 @@
     char* mark4 = str2;
     fgets(str2, max, stdin);
     if (isalpha(*mark3) != 0) {
-    while (isalpha(*mark4) != 0)
-    mark4++;
+        while (isalpha(*mark4) != 0)
+            mark4++;
     } 
     if (mainchek(mark3, wr) == 1)
-    return 0;
+        return 0;
     else
-    mark3 = mark4;
+        mark3 = mark4;
     translate(mark3, d2);
     calculation(d2);
 
@@ -51,5 +51,4 @@
     printf("\n");
     system("pause");
     return 0;
-    }
-
+}
