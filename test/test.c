d8ea83b9997fd3cda9cffb4413d982f98e235b9a
Svetlana <petri6284@gmail.com>
2022-05-22 17:12:00 +0700

    Update test.c

---
+++ test/test.c
@@ -0,0 +1,52 @@
+#include "ctest.h"
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "libgeometry/lib_main_chek.h"
+
+
+CTEST(input, word_chek_true)
+{
+	char* str = "circle(1 2,3)";
+	char* mark = str;
+	char* wr = "circle";
+	bool result = word_chek(wr, mark);
+	ASSERT_TRUE(result);
+}
+CTEST(input, search_comma_true)
+{
+	char* str = "circle(1 2,3)";
+	char* mark = str;
+	bool result = search_comma(mark);
+	ASSERT_TRUE(result);
+}
+CTEST(input, search_breckets_true)
+{
+	char* str = "circle(1 2,3)";
+	char* mark = str;
+	bool result = search_breckets(mark);
+	ASSERT_TRUE(result);
+}
+CTEST(input, word_chek_false)
+{
+	char* str = "cirle123";
+	char* mark = str;
+	char* wr = "circle";
+	bool result = word_chek(wr, mark);
+	ASSERT_FALSE(result);
+}
+CTEST(input, search_comma_false)
+{
+	char* str = "cirle123";
+	char* mark = str;
+	bool result = search_comma(mark);
+	ASSERT_TRUE(result);
+}
+CTEST(input, search_breckets_false)
+{
+	char* str = "cirle123";
+	char* mark = str;
+	bool result = search_breckets(mark);
+	ASSERT_TRUE(result);
+}
+
